a9989e64f97e6ecbf01d8bec9b037805eeff366f
dantvi <daniel.tving@gmail.com>
2024-08-23 17:56:43 +0200

    searching and filtering monsters

--- src/App.js
+++ src/App.js
@@ -9,10 +9,12 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
+    console.log('constructor');
   }
 
   componentDidMount() {
-    {/* Fetch monsters from API */}
+    console.log('componentDidMount');
+    /* Fetch monsters from API */
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) =>
@@ -28,8 +30,24 @@ class App extends Component {
   }
 
   render() {
+    console.log('render');
     return (
       <div className="App">
+        <input 
+          className='search-box' 
+          type='search' 
+          placeholder='search monsters' 
+          onChange={(event) => { 
+            console.log(event.target.value)
+            const searchString = event.target.value.toLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLowerCase().includes(searchString);
+            });
+            
+            this.setState(() => {
+              return { monsters: filteredMonsters };
+            });
+          }} />
         {
           this.state.monsters.map((monster) => {
             return (
