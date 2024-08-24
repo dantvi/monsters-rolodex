d26b35ff9fc4bd5f045124d7b009a67e4d8489a7
dantvi <daniel.tving@gmail.com>
2024-08-24 18:30:17 +0200

    card list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 
+import CardList from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -10,11 +11,9 @@ class App extends Component {
       monsters: [],
       searchField: '',
     };
-    console.log('constructor');
   }
 
   componentDidMount() {
-    console.log('componentDidMount');
     /* Fetch monsters from API */
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
@@ -22,9 +21,6 @@ class App extends Component {
         this.setState(
           () => {
             return { monsters: users }
-          },
-          () => {
-            console.log(this.state);
           }
         )
       );
@@ -38,8 +34,7 @@ class App extends Component {
   }
 
   render() {
-    console.log('render');
-
+    /* console.log('render from AppJS'); */
     const { monsters, searchField } = this.state;
     const { onSearchChange } = this;
 
@@ -55,15 +50,7 @@ class App extends Component {
           placeholder='search monsters' 
           onChange={onSearchChange} 
         />
-        {
-          filteredMonsters.map((monster) => {
-            return (
-              <div key={monster.id}>
-                <h1>{monster.name}</h1>
-              </div>
-            );
-          })
-        }
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
